57775f2f73ca450f6f5c8473b9f8874103549eaf
SofiaFredriksson <sofiapkf@gmail.com>
2019-08-27 11:51:21 +0100

    weathercard renders weather based on country  from search

--- src/App.js
+++ src/App.js
@@ -3,11 +3,48 @@ import Nav from './containers/Nav'
 import CitySearchPage from './containers/CitySearchPage'
 import WeatherCard from './components/WeatherCard'
 import './App.css';
-
+const weathers = [
+    {country: 'london', 
+    description: "scattered clouds",
+    icon: "03d",
+    id: 802,
+    main: "Clouds",
+    temperature: 25.08,
+    temp_max: 28.89,
+    temp_min: 22.22 
+  },
+  {country: 'hook', 
+    description: "clear sky",
+    icon: "01d",
+    id: 800,
+    main: "Clear",
+    temperature: 24.02,
+    temp_max: 26.67,
+    temp_min: 21
+  },
+  {country: 'stockholm',
+    description: "clear sky",
+    icon: "01d",
+    id: 800,
+    main: "Clear",
+    temperature: 23.93,
+    temp_max: 25,
+    temp_min: 23
+  },
+  {country: 'geneva',
+    description: "clear sky",
+    icon: "01d",
+    id: 800,
+    main: "Clear",
+    temperature: 23.05,
+    temp_max: 26.11,
+    temp_min: 20
+  }
+]
 export default class App extends Component {
   state = {
     page: 'home',
-    country: ''
+    country: '',
   }
 
   handleSubmit = () => {
@@ -21,24 +58,26 @@ export default class App extends Component {
         country: event.target.value
     })
 }
-
-  rednerSwitch = () => {
+  filterWeather = (weathers) => {
+    return weathers.find(weather => weather.country.toLowerCase().includes(this.state.country.toLowerCase()))
+  }
+  renderSwitch = () => {
     switch(this.state.page) {
       case 'home':
         return <CitySearchPage handleSubmit={this.handleSubmit} handleChange={this.handleChange}/>;
       case 'weather':
-        return <WeatherCard/>;  
+        return <WeatherCard weather={this.filterWeather(weathers)}/>;  
       default:
         return null;
     }
   }
   render() {
-    console.log(this.state.page);
+    console.log(this.state.country);
     
     return (
       <div className="App">
         <Nav/>
-        { this.rednerSwitch() }
+        { this.renderSwitch() }
       </div>
     );
   }
--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
-export default function WeatherCard() {
+export default function WeatherCard(props) {
+    console.log(props);
+    
     return (
         <div className="weather-card">
-            hello
+            Goodmorning, the weather in 
+            <h1>{props.weather.country.toUpperCase()}</h1>
+            <p>is mainly {props.weather.description}</p>
+            <p>with a temperature of {props.weather.temperature}ºC.</p>
+            <p>You can expect highs of {props.weather.temp_max}ºC</p>
+            <p>and lows of {props.weather.temp_min}ºC</p>
+
+
         </div>
     )
 }
